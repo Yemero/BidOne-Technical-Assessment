e812b2852867341043d2020f40fe392fe636b29f
Kevin Lee <kevinjlee796@gmail.com>
2026-05-19 17:20:16 +1200

    Add Required attributes to Model
    FirstName and LastName with [Required] in FormSubmission to enforce model validation.

--- FormApp.API/Models/FormSubmission.cs
+++ FormApp.API/Models/FormSubmission.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FormApp.API.Models
 {
     public class FormSubmission
     {
+        [Required]
         public string FirstName { get; set; } = string.Empty;
+        [Required]
         public string LastName { get; set; } = string.Empty;
         public DateTime SubmittedAt { get; set; } = DateTime.UtcNow;
     }
